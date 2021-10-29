a61db9e99bec639ee87c88a47bcf4d1cc22cf7df
Alexander Petrov <57758788+K1ndum@users.noreply.github.com>
2021-10-29 18:38:22 +0300

    feat: Implemented 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+let fs = require('fs');
+const path = require('path');
 
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  const files = fs.readdir(folder, { withFileTypes: true },
+    (err, files) => {
+    console.log("\n Start copy file:");
+    if (err)
+      console.log(err);
+    else {  
+      fs.access(folderCopy, fs.constants.F_OK, (err) => {
+        console.log('\nChecking for the presence of a folder');
+        if (err) {  
+          fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+            if (err) {
+              throw err
+            } else {
+              console.log('Folder created')
+            }
+          });
+        } else {
+          console.log('The folder exists');
+        }
+        files.forEach(file => {
+          const pathToFile = path.join(__dirname, `files/${file.name}`);
+          const pathToFileCopy = path.join(__dirname, `files-copy/${file.name}`);
+          fs.createReadStream(pathToFile).pipe(fs.createWriteStream(pathToFileCopy));
+        })
+  
+        console.log('Files copied successfully');
+      });
+    }
+  })
+} 
+
+copyDir();
